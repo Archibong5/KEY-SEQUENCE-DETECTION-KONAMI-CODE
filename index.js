b26a34970c0b6a6f5f16e238ee7a420ce4a69c0a
Archibong5 <109733824+Archibong5@users.noreply.github.com>
2023-04-17 20:58:43 +0100

    Add initial konami code website file

---
+++ index.js
@@ -0,0 +1,18 @@
+const pressed = [];
+const secretCode = "nzaki"
+
+
+
+
+window.addEventListener("keyup", (e) =>{
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+
+  if(pressed.join('').includes(secretCode)){
+    console.log("DING DING");
+    cornify_add();
+  };
+  console.log(pressed);
+
+
+})
